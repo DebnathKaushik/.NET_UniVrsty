66883b16de10452df71e2e5474cb7c237ba422e7
DebnathKaushik <debnathkaushik49@gmail.com>
2025-08-22 10:40:53 +0600

    ls

---
+++ IntroAPI/IntroAPI/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class StudentController : ApiController
+    {
+        [HttpGet]
+        [Route("all-student")]
+        public HttpResponseMessage get_all_student()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Get all the Student");
+        }
+
+        [HttpPost]
+        [Route("student-create")]
+        public HttpResponseMessage create_student()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "create Student");
+        }
+
+        [HttpDelete]
+        [Route("student-delete/{id}")]
+        public HttpResponseMessage delete_student(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Delete Student: "+id);
+        }
+
+        [HttpGet]
+        [Route("student/{id}")]
+        public IHttpActionResult Specific_student(int id)
+        {
+            return Ok("Student id: "+id);
+        }
+
+    }
+}
---
+++ ZeroHunger/ZeroHunger/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class LoginController : Controller
+    {
+        Zero_HungerEntities6 db = new Zero_HungerEntities6();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RestaurantLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RestaurantLogin(string name,string pass)
+        {
+            // Restaurant exist or Not
+            var restaurant = (from u in db.Restaurants
+                        where
+                       u.Restaurant_Name.Equals(name) &&
+                       u.restaurant_Pass.Equals(pass)
+                        select u).SingleOrDefault();
+            if (restaurant != null)
+            {
+                Session["Restaurant_Id"] = restaurant.Restaurant_Id;
+                Session["Restaurant_Name"] = restaurant.Restaurant_Name;
+
+                return RedirectToAction("Index","Restaurant");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid username or password";
+                return View();
+            }
+
+        }
+
+    }
+}
