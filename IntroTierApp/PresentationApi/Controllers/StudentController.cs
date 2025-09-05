4c73016d00ae5e4e1401e33a5d8c2ba464ede470
DebnathKaushik <debnathkaushik49@gmail.com>
2025-09-05 22:28:35 +0600

    ThreeTire/Layer architecture

---
+++ IntroTierApp/PresentationApi/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+
+namespace PresentationApi.Controllers
+{
+    [RoutePrefix("api/student")]
+    public class StudentController : ApiController
+    {
+        [HttpGet]
+        [Route("all")]
+        public HttpResponseMessage Get()
+        {
+            var data = StudentService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpGet]
+        [Route("sch")]
+        public HttpResponseMessage GetSch()
+        {
+            var data = StudentService.GetSch();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpPost]
+        [Route("create")]
+        public HttpResponseMessage Create(StudentDTO s)
+        {
+            var data = StudentService.Create(s);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+
+        }
+    }
+}
---
+++ IntroTierApp/PresentationApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PresentationApi.WebApiApplication" Language="C#" %>
