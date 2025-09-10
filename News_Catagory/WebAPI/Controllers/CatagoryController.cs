bc3659920a3003e19d74700c8a695cb7e48909c2
DebnathKaushik <debnathkaushik49@gmail.com>
2025-09-10 10:27:55 +0600

    catagory update & delete

---
+++ News_Catagory/WebAPI/Controllers/CatagoryController.cs
@@ -0,0 +1,44 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class CatagoryController : ApiController
+    {
+        [HttpPatch]
+        [Route("catagory/update/{id}")]
+        public HttpResponseMessage update(int id, CatagoryDTO c)
+        {
+             
+            c.Id = id;
+
+            var data = CatagoryService.updateCatagory(c);
+            if (data)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "Catagory updated successfully");
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound, "Catagory not found");
+
+        }
+
+        [HttpDelete]
+        [Route("catagory/delete/{id}")]
+        public HttpResponseMessage delete(int id)
+        {
+            var data = CatagoryService.deleteCatagory(id);
+            if (data)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "Deleted successfully");
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound, "Catagory not found");
+        }
+    }
+}
